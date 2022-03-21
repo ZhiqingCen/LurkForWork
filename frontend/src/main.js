ecbf80d1957a2910bc5bdbdee0aaabe3df088a19
Zhiqing <zhiqingcen.zc@gmail.com>
2022-03-21 22:39:40 +1100

    registration & login: draft update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,51 @@ import { BACKEND_PORT } from './config.js';
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
 
+let authToken = null;
+let authUserId = null;
 
+const apiCall = (path, requestBody) => {
+    const init = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+    };
 
-const registerBtn = document.getElementById("register-btn");
-registerBtn.addEventListener("click", () => {
+    fetch(`http://localhost:${BACKEND_PORT}/${path}`, init)
+        .then(response => response.json())
+        .then(body => {
+            if (body.error) {
+                alert(body.error); // TODO
+            } else {
+                authToken = body.token;
+                authUserId = body.userId;
+                toggleScreenWelcome();
+            }
+        });
+}
+
+const register = (email, password, name) => {
+    apiCall("auth/register", {
+        email,
+        password,
+        name,
+    });
+};
+
+const login = (email, password) => {
+    apiCall("auth/login", {
+        email,
+        password,
+    });
+};
+
+const getProfile = (userId) => {
+    console.log(userId); // TODO
+};
+
+document.getElementById("register-btn").addEventListener("click", () => {
     const registerEmail = document.getElementById("register-email").value;
     const registerName = document.getElementById("register-name").value;
     const registerPassword = document.getElementById("register-password").value;
@@ -16,29 +57,39 @@ registerBtn.addEventListener("click", () => {
         return;
     }
 
-    const requestBody = {
-        email: registerEmail,
-        password: registerPassword,
-        name: registerName,
-    };
+    register(registerEmail, registerPassword, registerName);
 
-    const init = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-    };
-
-    fetch(`http://localhost:${BACKEND_PORT}/auth/register`, init)
-        .then(response => response.json())
-        .then(body => {
-            if (body.error) {
-                alert(body.error);
-            } else {
-                console.log(`body is ${body}`);
-            }
-        });
-
-    console.log("register");
+    console.log("register"); // TODO
 });
+
+document.getElementById("login-btn").addEventListener("click", () => {
+    const loginEmail = document.getElementById("login-email").value;
+    const loginPassword = document.getElementById("login-password").value;
+    
+    login(loginEmail, loginPassword);
+
+    console.log("login"); // TODO
+});
+
+const toggleScreenRegister = () => {
+    document.getElementById("screen-register").style.display = "block";
+    document.getElementById("screen-login").style.display = "none";
+    document.getElementById("screen-welcome").style.display = "none";
+};
+
+const toggleScreenLogin = () => {
+    document.getElementById("screen-login").style.display = "block";
+    document.getElementById("screen-register").style.display = "none";
+    document.getElementById("screen-welcome").style.display = "none";
+};
+
+const toggleScreenWelcome = () => {
+    document.getElementById("screen-welcome").style.display = "block";
+    document.getElementById("screen-register").style.display = "none";
+    document.getElementById("screen-login").style.display = "none";
+    console.log("load the user"); // TODO
+    getProfile(authUserId);
+};
+    
+document.getElementById("nav-register").addEventListener("click", toggleScreenRegister);
+document.getElementById("nav-login").addEventListener("click", toggleScreenLogin);
